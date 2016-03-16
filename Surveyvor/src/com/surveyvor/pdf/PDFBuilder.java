6c6748228be5c55c7d226606f2907ecfa4c5a727
Laurent <Laurent@192.168.1.97>
2016-03-16 17:54:46 +0100

    ajout generate pdf

--- Surveyvor/src/com/surveyvor/pdf/PDFBuilder.java
+++ Surveyvor/src/com/surveyvor/pdf/PDFBuilder.java
@@ -2,10 +2,11 @@ package com.surveyvor.pdf;
 
 import java.util.List;
 import java.util.Map;
- 
+import java.util.Map.Entry;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
+
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Font;
@@ -15,65 +16,63 @@ import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
- 
-/**
- * This view class generates a PDF document 'on the fly' based on the data
- * contained in the model.
- * @author www.codejava.net
- *
- */
+import com.surveyvor.model.User;
+
 public class PDFBuilder extends AbstractITextPdfView {
- 
-    @Override
-    protected void buildPdfDocument(Map<String, Object> model, Document doc,
-            PdfWriter writer, HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
-        // get data model which is passed by the Spring container
-        List<Book> listBooks = (List<Book>) model.get("listBooks");
-         
-        doc.add(new Paragraph("Recommended books for Spring framework"));
-         
-        PdfPTable table = new PdfPTable(5);
-        table.setWidthPercentage(100.0f);
-        table.setWidths(new float[] {3.0f, 2.0f, 2.0f, 2.0f, 1.0f});
-        table.setSpacingBefore(10);
-         
-        // define font for table header row
-        Font font = FontFactory.getFont(FontFactory.HELVETICA);
-        font.setColor(BaseColor.WHITE);
-         
-        // define table header cell
-        PdfPCell cell = new PdfPCell();
-        cell.setBackgroundColor(BaseColor.BLUE);
-        cell.setPadding(5);
-         
-        // write table header
-        cell.setPhrase(new Phrase("Book Title", font));
-        table.addCell(cell);
-         
-        cell.setPhrase(new Phrase("Author", font));
-        table.addCell(cell);
- 
-        cell.setPhrase(new Phrase("ISBN", font));
-        table.addCell(cell);
-         
-        cell.setPhrase(new Phrase("Published Date", font));
-        table.addCell(cell);
-         
-        cell.setPhrase(new Phrase("Price", font));
-        table.addCell(cell);
-         
-        // write table row data
-        for (Book aBook : listBooks) {
-            table.addCell(aBook.getTitle());
-            table.addCell(aBook.getAuthor());
-            table.addCell(aBook.getIsbn());
-            table.addCell(aBook.getPublishedDate());
-            table.addCell(String.valueOf(aBook.getPrice()));
-        }
-         
-        doc.add(table);
-         
-    }
- 
+
+	@Override
+	protected void buildPdfDocument(Map<String, Object> model, Document doc,
+			PdfWriter writer, HttpServletRequest request, HttpServletResponse response)
+					throws Exception {
+		// get data model which is passed by the Spring container
+		Map<Long,List<User>> resultat = (Map<Long, List<User>>) model.get("resultat");
+
+		doc.add(new Paragraph("Resultat du sondage"));
+
+		PdfPTable table = new PdfPTable(2);
+		table.setWidthPercentage(100.0f);
+		table.setWidths(new float[] {4.0f,6.0f });
+		table.setSpacingBefore(10); 
+
+		// define font for table header row
+		Font font = FontFactory.getFont(FontFactory.HELVETICA);
+		font.setColor(BaseColor.BLACK);
+
+		// define table header cell
+		PdfPCell cell = new PdfPCell();
+		cell.setBackgroundColor(BaseColor.ORANGE);
+		cell.setPadding(5);
+
+		// write table header
+		cell.setPhrase(new Phrase("Groupes", font));
+		table.addCell(cell);
+
+		cell.setPhrase(new Phrase("Etudiants affectés", font));
+		table.addCell(cell);
+
+		// write table row data
+		//Parcours de la map
+		for(Entry<Long, List<User>> entry : resultat.entrySet()) {
+			//cle est le groupe
+			//valeur est la liste d'User affectée au dit groupe
+			Long cle = entry.getKey();
+			List<User> valeur = entry.getValue();
+
+			//affiche l'id du groupe dans une cellule
+			table.addCell(String.valueOf(entry.getKey()));
+
+			//construit un paragraphe de string étant les noms d'user de la liste affecté a ce groupe
+			Paragraph p = new Paragraph();
+			for (int j=0;j<valeur.size(); j++) {
+				p.add(valeur.get(j).getName()+"\n");
+			}
+			//ajoute le parapgrahe a la cellule
+			table.addCell(p);
+		}
+
+		//ajoute la table au document PDF
+		doc.add(table);
+
+	}
+
 }
