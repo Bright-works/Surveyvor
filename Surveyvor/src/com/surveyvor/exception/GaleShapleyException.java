86437f0f24faf47c2a6aec30c06664a097166915
boukris <sarahboukris@hotmail.fr>
2016-03-17 14:46:41 +0100

    GaleShapleyException

---
+++ Surveyvor/src/com/surveyvor/exception/GaleShapleyException.java
@@ -0,0 +1,13 @@
+package com.surveyvor.exception;
+
+public class GaleShapleyException extends Exception {
+	private static final long serialVersionUID = -6833705333904051331L;
+
+	public GaleShapleyException(String value) {
+		super(value);
+	}
+
+	public GaleShapleyException() {
+		super();
+	}
+}
