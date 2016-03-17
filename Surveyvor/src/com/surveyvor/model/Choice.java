41f51efba7a97499aedf4be1a419905b7d7925c4
boukris <sarahboukris@hotmail.fr>
2016-03-17 08:35:46 +0100

    le mien

--- Surveyvor/src/com/surveyvor/model/Choice.java
+++ Surveyvor/src/com/surveyvor/model/Choice.java
@@ -1,5 +1,7 @@
 package com.surveyvor.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,8 +14,13 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-public class Choice {
+public class Choice implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7469577458867792848L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "Id_Choice")
